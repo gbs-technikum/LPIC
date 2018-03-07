32cf0890db6c65d10b96a8df090c1306e5930965
Michael König <m.koenig@go2k.de>
2018-03-07 11:44:55 +0100

    infoActivity implemented

---
+++ app/src/main/java/lpictraineeteacher/project/local/lpic_trainee_teacher/acivitysMain/InfoActivity.java
@@ -0,0 +1,15 @@
+package lpictraineeteacher.project.local.lpic_trainee_teacher.acivitysMain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import lpictraineeteacher.project.local.lpic_trainee_teacher.R;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
